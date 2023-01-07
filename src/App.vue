8ccce0a7da5889f27a6da4306295edfec6c9f16f
immortalmice <killer536697@gmail.com>
2023-01-07 12:10:42 +0800

    Feat: Background & icon

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,17 @@
 </template>
 
 <style lang="scss">
+body {
+  background-image: url('@/assets/background.jpg');
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
