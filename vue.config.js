a0a7f9e8621b5031510529420c423ba3c2ba8819
immortalmice <killer536697@gmail.com>
2023-01-07 17:53:21 +0800

    Build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     outputDir: './docs',
+    publicPath: './',
     devServer: {
         port: 8080,
         open: {
