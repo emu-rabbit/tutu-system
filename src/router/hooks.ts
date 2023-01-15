de7b29310a6cf8f56f7e594d50ae5ac12c27b857
immortalmice <killer536697@gmail.com>
2023-01-15 22:22:20 +0800

    Feat: Block enter

--- src/router/hooks.ts
+++ src/router/hooks.ts
@@ -1,5 +1,7 @@
 import router from './index'
 import nprogress from 'nprogress'
+import useStore from '@/store'
+import { info } from '@/apis/Auth'
 
 nprogress.configure({
     showSpinner: false
@@ -7,6 +9,18 @@ nprogress.configure({
 
 router.beforeEach(async (to, from, next) => {
     nprogress.start()
+    const store = useStore()
+    try {
+        const { data } = await info()
+        store.user = {
+            email: data.email,
+            username: data.username,
+            userGroup: data.user_groups
+        }
+    } catch (e) {
+        console.log('Info api failed')
+        store.user = null
+    }
     next()
 })
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,13 @@
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', {
-    state: () => ({ count: 0 }),
-    getters: {
-        double: state => state.count * 2
-    },
-    actions: {
-        increment () {
-            this.count++
-        }
-    }
+const getDefaultState = () => ({
+    user: null as {
+        email: string,
+        username: string,
+        userGroup: string[]
+    } | null
+})
+
+export default defineStore('main', {
+    state: getDefaultState
 })
